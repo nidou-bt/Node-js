8b80c512da08c2702930f2e9d8780d2f48908bd0
nidhal-bt <bentaaritn@gmail.com>
2024-01-07 14:46:30 +0100

    add lib/data

--- app/lib/data.js
+++ app/lib/data.js
@@ -12,8 +12,13 @@ var lib = {};
 lib.baseDir = path.join(__dirname, "/../.data/");
 
 // Write data to a file
+/**
+ * @param dir
+ * folder parent
+ */
 lib.create = function (dir, file, data, callback) {
   // Open the file for writting
+  // 'wx': Open file for writing. The file is created (if it does not exist) or fails if the path exists
   fs.open(
     lib.baseDir + dir + "/" + file + ".json",
     "wx",
@@ -41,6 +46,76 @@ lib.create = function (dir, file, data, callback) {
       }
     }
   );
+};
+
+// Read data from a file
+/**
+ * @param dir
+ * folder parent
+ */
+lib.read = function (dir, file, callback) {
+  fs.readFile(
+    lib.baseDir + dir + "/" + file + ".json",
+    "utf-8",
+    function (err, data) {
+      callback(err, data);
+    }
+  );
+};
+
+// Update data from a file
+/**
+ * @param dir
+ * folder parent
+ */
+lib.update = function (dir, file, data, callback) {
+  // Open the file for writting
+  // 'r+': Open file for reading and writing. An exception occurs if the file does not exist.
+  fs.open(
+    lib.baseDir + dir + "/" + file + ".json",
+    "r+",
+    function (err, fileDescriptor) {
+      if (!err && fileDescriptor) {
+        // Convert data to string
+        var stringData = JSON.stringify(data);
+
+        // Truncate the file
+        fs.truncate(fileDescriptor, function (err) {
+          if (!err) {
+            // write to the file and close it
+            fs.writeFile(fileDescriptor, stringData, function (err) {
+              if (!err) {
+                fs.close(fileDescriptor, function (err) {
+                  if (!err) {
+                    callback(false);
+                  } else {
+                    callback("Error closing existing file");
+                  }
+                });
+              } else {
+                callback("Error writing to existing file");
+              }
+            });
+          } else {
+            callback("Error truncating file");
+          }
+        });
+      } else {
+        callback("Could not open the file for updating, it may not existe yet");
+      }
+    }
+  );
+};
+
+lib.delete = function (dir, file, callback) {
+  // Unlink the file
+  fs.unlink(lib.baseDir + dir + "/" + file + ".json", function (err) {
+    if (!err) {
+      callback("false");
+    } else {
+      callback("Error deleting file");
+    }
+  });
 };
 
 module.exports = lib;
